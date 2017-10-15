2739f23f29298b60cdb367733ec487408fa9373d
marigbede <marigbede@codesistance.com>
2017-10-15 23:26:08 +0100

    Add project files.

---
+++ PatientDataAdministration.Web/Areas/ClientOperations/ClientOperationsAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace PatientDataAdministration.Web.Areas.ClientOperations
+{
+    public class ClientOperationsAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "ClientOperations";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "ClientOperations_default",
+                "ClientOperations/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ PatientDataAdministration.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PatientDataAdministration.Web.Global" Language="C#" %>
