0d3c9e36dfcf171d9b14649e1e1e9f1bfc2a026e
marigbede <marigbede@codesistance.com>
2017-10-17 09:06:10 +0100

    Test Commit

---
+++ PatientDataAdministration.EnumLibrary/Status.cs
@@ -0,0 +1,12 @@
+﻿using PatientDataAdministration.EnumLibrary.Dictionary;
+
+namespace PatientDataAdministration.EnumLibrary
+{
+    public enum Status : int
+    {
+        [EnumDisplayName(DisplayName = "Active")]
+        Active = 1,
+        [EnumDisplayName(DisplayName = "Banned")]
+        Banned
+    }
+}
