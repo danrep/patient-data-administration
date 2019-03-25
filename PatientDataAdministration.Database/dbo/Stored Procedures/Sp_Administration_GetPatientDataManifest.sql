b73097409ce905ab0a611c98d8c196fc831e24e2
marigbede <marigbede@codesistance.com>
2019-03-25 23:52:12 +0100

    Stored Procedure Update

---
+++ PatientDataAdministration.Database/dbo/Stored Procedures/Sp_Administration_GetPatientDataManifest.sql
@@ -0,0 +1,28 @@
+﻿CREATE PROCEDURE [dbo].[Sp_Administration_GetPatientDataManifest]
+	@stateId INT, 
+	@siteId INT, 
+	@restrictOnBiometrics BIT = 'false', 
+	@startDate DATETIME, 
+	@endDate DATETIME
+AS
+	if (@siteId = 0)
+	begin
+		set @siteId = null
+	end
+
+	if (@restrictOnBiometrics = 'true')
+	begin
+		select ppi.*, ppbd.DateRegistered BiometricRegistrationDate, CONVERT(bit, 1) HasBioMetrics from Patient_PatientInformation ppi 
+		inner join Patient_PatientBiometricData ppbd on ppi.PepId = ppbd.PepId
+		where ppi.SiteId in 
+		(select Id from Administration_SiteInformation where StateId = @stateId and Id = ISNULL(@siteId, Administration_SiteInformation.Id))
+		and ppi.WhenCreated > @startDate and ppi.WhenCreated < @endDate
+	end
+	else
+	begin
+		select ppi.*, ISNULL(ppbd.DateRegistered, N'1900-01-01') BiometricRegistrationDate, CONVERT(bit, (CASE WHEN ISNULL(ppbd.DateRegistered, N'1900-01-01') = N'1900-01-01' THEN 0 ELSE 1 END)) HasBioMetrics from Patient_PatientInformation ppi 
+		left join Patient_PatientBiometricData ppbd on ppi.PepId = ppbd.PepId
+		where ppi.SiteId in 
+		(select Id from Administration_SiteInformation where StateId = @stateId and Id = ISNULL(@siteId, Administration_SiteInformation.Id))
+		and ppi.WhenCreated > @startDate and ppi.WhenCreated < @endDate
+	end
